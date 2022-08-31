2c43a3e021fa97b81a26138b029ba4817933657d
Morten Vestergaard <mort286f@zbc.dk>
2022-08-31 13:16:19 +0200

    Added genre class

---
+++ src/app/models/genre.ts
@@ -0,0 +1,4 @@
+export class Genre {
+  movieId: number | undefined;
+  genreId: number | undefined;
+}
--- src/app/models/movie.ts
+++ src/app/models/movie.ts
@@ -1,9 +1,11 @@
+import { Genre } from "./genre";
+
 export class Movie {
   id: number | undefined;
   poster: string | undefined;
   name: string | undefined;
   description: string | undefined;
-  genres: any;
+  genres: Array<Genre> | undefined
   releaseDate: string | undefined;
   runtime: string | undefined;
 }
