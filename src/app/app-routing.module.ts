037abb512f7bdb6ea19917de014d5005c6eec8b4
Morten Vestergaard <mort286f@zbc.dk>
2022-08-31 12:13:09 +0200

    updated insert-movie and some more

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InsertMovieComponent } from './insert-movie/insert-movie.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'insertmovie', component: InsertMovieComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
